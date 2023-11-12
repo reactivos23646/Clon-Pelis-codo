898315fd93ded35aa2d079884765cfd9a5f118f2
Mariano Mercevich <marianomercevich@gmail.com>
2023-11-11 23:26:48 -0300

    cfg- testing test

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-
 import App from './App';
 import { describe, expect } from 'vitest';
 import { it } from 'vitest';
@@ -11,17 +10,5 @@ describe('App', () => {
 
 
 
-/*   it('renders headline', () => {
-    
-    const component =  render(<App />);
-
-   const heading = component.getByRole ('heading', {name:"Hola Mundo!!!"  });
-
-   expect (heading.textContent).toBe("Hola Mundo!!!" ); 
-   // se suele usar la exprecion regular mas que el string /hola mundo!!/i lo encontrtaria sin importar las may y min
-  
-   expect (heading).toBeTruthy() */
-
-    // check if App components renders headline
   });
 }); 
--- tests/setup.js
+++ tests/setup.js
@@ -2,9 +2,7 @@ import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
-tests: {
-    environment: 'jsdom',
-    afterEach(() => {
-        cleanup();
-      });
-  };
+
+afterEach(() => {
+  cleanup();
+});
