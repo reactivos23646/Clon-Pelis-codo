8367fc9fc68df6104558f89516aa5149b0ca9aa8
Mariano Mercevich <marianomercevich@gmail.com>
2023-11-11 21:31:17 -0300

    test qa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>Test QA!!!</div>
+    <div>
+      <h1>Test QA!!!</h1>
+      <h2> prueba 2!!!</h2>
+      
+    </div>
   )
 }
 export default App
