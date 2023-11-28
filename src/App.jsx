d6c1ac4e60f61193b7e5336b26e75e956aa9849c
luisperrone <111819839+luisperrone@users.noreply.github.com>
2023-11-28 19:17:40 -0300

    Boceto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Datepicker, Input, initTE } from "tw-elements";
+import React, { useEffect } from "react";
+import YourComponent from './components/Modal';
 
 export const App = () => {
+  useEffect(() => {
+    initTE({ Datepicker, Input });
+  }, []);
   return (
-    <div>Test MAIN!!!</div>
+    <>
+    <YourComponent/>
+    </>
   )
 }
 
 export default App
+
+/* <div>Test MAIN!!!</div> */
---
+++ src/components/Modal.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+
+const YourComponent = () => {
+  return (
+    <div className="container my-24 mx-auto md:px-6">
+      {/* Section: Design Block */}
+      <section className="mb-32">
+        <h2 className="mb-12 text-center text-3xl font-bold">
+          MODAL
+        </h2>
+
+        <div className="flex flex-wrap items-center">
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:px-6 lg:mb-0 lg:w-5/12">
+            <div className="relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20" data-te-ripple-init data-te-ripple-color="light">
+              <img src="https://mdbcdn.b-cdn.net/img/new/textures/full/98.jpg" className="w-full" />
+              <a href="#!">
+                <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,0%,0.4)] bg-fixed">
+                  <div className="flex h-full items-center justify-center">
+                    <div className="px-6 py-12 text-center text-white md:px-12">
+                      <h3 className="mb-6 text-2xl font-bold uppercase">
+                        Nomre de la Pelicula
+                        <u className="text-[hsl(210,12%,80%)]">Play icono</u>
+                      </h3>
+                      <p className="text-[hsl(210,12%,80%)]">
+                        Aca puede ir de que va la pelicula
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                  <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div className="w-full shrink-0 grow-0 basis-auto md:px-6 lg:w-7/12">
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">Rating</p>
+              </div>
+            </div>
+
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">Genero</p>
+              </div>
+            </div>
+
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">Clasificacion</p>
+              </div>
+            </div>
+
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">Duracion</p>
+              </div>
+            </div>
+
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">Genero</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nihil quisquam quibusdam modi sapiente magni molestias
+                  pariatur facilis reprehenderit facere aliquam ex.
+                </p>
+              </div>
+            </div>
+
+            <div className="mb-12 flex">
+              <div className="shrink-0">
+                <div className="rounded-md bg-[hsl(204,30%,20%)] p-4 shadow-lg dark:bg-[hsl(204,23%,30%)]">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="h-6 w-6 text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                  </svg>
+                </div>
+              </div>
+              <div className="ml-4 grow">
+                <p className="mb-1 font-bold">resena</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nihil quisquam quibusdam modi sapiente magni molestias
+                  pariatur facilis reprehenderit facere aliquam ex.
+                </p>
+              </div>
+            </div>
+
+            {/* Add more converted JSX for other items as needed */}
+
+          </div>
+        </div>
+      </section>
+      {/* Section: Design Block */}
+    </div>
+  );
+}
+
+export default YourComponent;
