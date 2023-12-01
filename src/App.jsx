95a9fed7f5900dcbc2a4c5212493341d70b14b42
Isabel Bracho <127256795+ishaba1@users.noreply.github.com>
2023-12-01 17:15:02 -0300

    Cambios en App jsx
    en el <h1> y en el <h3> Cambio de mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const App = () => {
   return (
     <div>
-      <h1>Test PROD!!!</h1>
+      <h1>Esto es una Prueba de Branch</h1>
       <h2>prueba FUNCIONAL!!!</h2>
-      <h3>branch de prod</h3>
+      <h3>branch Dev-Isa</h3>
 
     </div>
   )
